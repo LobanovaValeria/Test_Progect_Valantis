059cb3ced3e70b69be17ac1b8c3a7fad883cc054
LobanovaValeria <valeriashishmakova.8@gmail.com>
2024-03-11 14:34:59 +0300

    second commit

--- src/components/Content/index.jsx
+++ src/components/Content/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from 'react';
+import React, { useState, useEffect, } from 'react';
 import Items from './Items';
 import DefaultPagination from './DefaultPagination';
 import { getIdsForAllPages, getIds, getItem } from '../../services/dataService';
